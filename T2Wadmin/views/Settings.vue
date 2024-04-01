f36bc551ec9586d6e58c4c3290954cfea03ff084
Kowebtech Ict <info@koweb.com.ng>
2024-04-01 12:52:58 +0100

    Update 1/4/24

--- T2Wadmin/views/Settings.vue
+++ T2Wadmin/views/Settings.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6 mb-4">
             <div class="card">
                 <div class="card-header">
                     Loan options
@@ -10,7 +10,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 mb-4">
             <div class="card">
                 <div class="card-header">
                     Thrift options
@@ -20,12 +20,43 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-6 mb-4">
+            <div class="card">
+                <div class="card-header">
+                    Shares options
+                </div>
+                <div class="card-body">
+                    <SetOption :fields="shares_options"></SetOption>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-4">
+            <div class="card">
+                <div class="card-header">
+                    Term deposit options
+                </div>
+                <div class="card-body">
+                    <SetOption :fields="td_options"></SetOption>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-4">
+            <div class="card">
+                <div class="card-header">
+                    Payout options
+                </div>
+                <div class="card-body">
+                    <SetOption :fields="payout_options"></SetOption>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
     import SetOption from '@/components/form/SetOption.vue'
     import { computed } from 'vue'
+    import * as yup from 'yup'
 
     const loan_options = computed(() => [
         {
@@ -54,8 +85,18 @@
             name: "loan_legibility"
         },
         {
+            label: "Max Credit Limit - Loan (%)",
+            type: "number",
+            name: "credit_limit"
+        },
+        {
             label: "Loan Tenure in Months (Seperate with commas)",
             name: "loan_tenure"
+        },
+        {
+            label: "Loan amount above balance requires guarantor",
+            as: "checkbox",
+            name: "require_guarantor"
         }
     ])
 
@@ -83,6 +124,74 @@
         {
             label: "Registration & Thrift Available",
             name: "regava"
+        }
+    ])
+
+    const shares_options = computed(() => [
+        {
+            label: "Price per unit of shares",
+            name: "share_unit",
+            rules: yup.number()
+        },
+        {
+            label: "Minimum Shares (in units)",
+            type: "number",
+            name: "min_shares"
+        },
+        {
+            label: "Maximum Shares (in units)",
+            type: "number",
+            name: "max_shares"
+        }
+    ])
+
+    const td_options = computed(() => [
+        {
+            label: "Term Deposit Rate",
+            name: "td_rate",
+            rules: yup.number()
+        },
+        {
+            label: "Minimum term deposit amount",
+            type: "number",
+            name: "min_td"
+        },
+        {
+            label: "Term Deposit Tenure in months(Seperate by commas)",
+            name: "td_tenure"
+        }
+    ])
+
+    const payout_options = computed(() => [
+        {
+            label: "Bank Charge (%)",
+            name: "bank_charges",
+            rules: yup.number()
+        },
+        {
+            label: "Maximum Cashout Request Per Member Per Month",
+            type: "number",
+            name: "max_monthly_payout"
+        },
+        {
+            label: "Minimum Cashout",
+            name: "minimum_cashout",
+            rules: yup.number()
+        },
+        {
+            label: "Cashout request days",
+            name: "crd",
+            as: "collection",
+            rows: 3,
+            fields: [
+                {
+                    label: "",
+                    name: "",
+                    as: "select",
+                    options: ["Monday", "Tuesday", "Wednesday", "Friday", "Saturday", "Sunday"],
+                    col: 12
+                }
+            ]
         }
     ])
 </script>
