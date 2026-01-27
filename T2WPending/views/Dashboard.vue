1bd71d5f1faba2448c4799a3ef7b10d49fee1cda
Kins Okafor <info@koweb.com.ng>
2026-01-27 18:22:48 +0100

    Refactors Dashboard layout for improved user experience and clarity

--- T2WPending/views/Dashboard.vue
+++ T2WPending/views/Dashboard.vue
@@ -1,10 +1,25 @@
 <template>
-    <div>
-        
+  <div class="row">
+    <div class="col-md-6 offset-md-3 text-center">
+      <h1>T2W Pending Dashboard</h1>
+      <p>Welcome, <u>{{ fullname }}</u>!</p>
+      <p>
+        You are yet to verify your account for Tokens2Wealth services. Check
+        your email <code>{{ user?.email }}</code> for verification instructions.
+        Please check both inbox and spam/junk folders.
+      </p>
+      <button class="btn btn-primary">
+        <i v-if="sending" class="fas fa-spinner fa-spin"></i>
+        Resend Verification Email
+    </button>
     </div>
+  </div>
 </template>
 <script setup>
-    
+import { useUserProfile } from "@/helpers";
+import { ref } from "vue";
+const sending = ref(false);
+
+const { user, fullname } = useUserProfile("self");
 </script>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
